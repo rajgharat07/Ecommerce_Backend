2267773da390b7290038cff8e6e19c978689f4f5
rajgharat07 <gharatyashraj@gmail.com>
2024-11-29 23:40:26 +0530

    Implement product controllers for managing product-related operations

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ const authToken = require("../middleware/authToken");
 const userLogout = require("../controllers/user/userLogout");
 const allUsers = require("../controllers/user/allUsers");
 const updateUser = require("../controllers/user/updateUser");
+const UploadProductController = require("../controllers/product/uploadProduct");
+const getProductController = require("../controllers/product/getProduct");
+const updateProductController = require("../controllers/product/updateProduct");
+const getCategoryProduct = require("../controllers/product/getCategoryProductOne");
+const getCategoryWiseProduct = require("../controllers/product/getCategoryWiseProduct");
 
 router.post("/signup",userSignUpController);
 router.post("/signin",userSignInController);
@@ -17,5 +22,12 @@ router.get("/userLogout",userLogout);
 //admin panel 
 router.get("/all-user",authToken,allUsers);
 router.post("/update-user",authToken,updateUser);
+
+//product
+router.post("/upload-product",authToken,UploadProductController);
+router.get("/get-product",getProductController);
+router.post("/update-product",authToken,updateProductController);
+router.get("/get-categoryProduct",getCategoryProduct);
+router.post("/category-product",getCategoryWiseProduct);
 
 module.exports = router;
