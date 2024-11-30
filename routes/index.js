b44631c9e152d612db0ea34cb148831e7ad53295
rajgharat07 <gharatyashraj@gmail.com>
2024-11-30 12:28:22 +0530

    Implemented controllers for addToCart,getProductDetails,countAddToCart,deleteAddToCart, and updateAddToCart operations

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,12 @@ const getProductController = require("../controllers/product/getProduct");
 const updateProductController = require("../controllers/product/updateProduct");
 const getCategoryProduct = require("../controllers/product/getCategoryProductOne");
 const getCategoryWiseProduct = require("../controllers/product/getCategoryWiseProduct");
+const getProductDetails = require("../controllers/product/getProductDetails");
+const addToCartController = require("../controllers/user/addToCartController");
+const countAddToCartProduct = require("../controllers/user/countAddToCartProduct");
+const addToCartViewProduct = require("../controllers/user/addToCartViewProduct");
+const updateAddToCartProduct = require("../controllers/user/updateAddToCartProduct");
+const deleteAddToCartProduct = require("../controllers/user/deleteAddToCartProduct");
 
 router.post("/signup",userSignUpController);
 router.post("/signin",userSignInController);
@@ -29,5 +35,14 @@ router.get("/get-product",getProductController);
 router.post("/update-product",authToken,updateProductController);
 router.get("/get-categoryProduct",getCategoryProduct);
 router.post("/category-product",getCategoryWiseProduct);
+router.post("/product-details",getProductDetails);
+
+
+//user add to cart
+router.post("/addtocart",authToken,addToCartController);
+router.get("/countAddToCartProduct",authToken,countAddToCartProduct);
+router.get("/view-card-product",authToken,addToCartViewProduct);
+router.post("/update-cart-product",authToken,updateAddToCartProduct);
+router.post("/delete-cart-product",authToken,deleteAddToCartProduct);
 
 module.exports = router;
