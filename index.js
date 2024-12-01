3af44930feb8daf77de9258d1e556f616b249aa9
rajgharat07 <gharatyashraj@gmail.com>
2024-12-01 10:39:17 +0530

    Bug fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
-app.use("/api",router)
+app.use("https://ecommerce-backend-gh2o.onrender.com/api",router)
 
 const PORT = 8080 || process.env.PORT;
 
