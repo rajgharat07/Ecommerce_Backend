359067a187ebebc5069052f53e1f4faf60612bd4
rajgharat07 <gharatyashraj@gmail.com>
2024-12-01 10:58:04 +0530

    Domain updated

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
-app.use("https://ecommerce-backend-gh2o.onrender.com/api",router)
+app.use("https://ecommerce-backend-1-7rjc.onrender.com/api",router)
 
 const PORT = 8080 || process.env.PORT;
 
